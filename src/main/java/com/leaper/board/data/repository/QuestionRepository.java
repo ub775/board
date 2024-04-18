f9ea0b95d5ef780f3a7cf80af63ee9abce13b3a3
ub775 <kmgni@naver.com>
2024-04-19 03:24:29 +0900

    Question 리포지토리 메소드 추가 (FindBySubject, FindBySubjectAndContent)

--- src/main/java/com/leaper/board/data/repository/QuestionRepository.java
+++ src/main/java/com/leaper/board/data/repository/QuestionRepository.java
@@ -4,4 +4,7 @@ import com.leaper.board.data.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject, String content);
 }
